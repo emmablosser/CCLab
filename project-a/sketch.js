b39262034861ba16d8c710816a67fef9c7be8bb0
emmablosser <emb9573@nyu.edu>
2025-03-16 12:26:23 +0800

    updated code + text

--- project-a/sketch.js
+++ project-a/sketch.js
@@ -5,11 +5,23 @@ let fish2X, fish2Y, fish2SpeedX, fish2SpeedY;
 let fish3X, fish3Y, fish3SpeedX, fish3SpeedY;
 let fish4X, fish4Y, fish4SpeedX, fish4SpeedY;
 let fish5X, fish5Y, fish5SpeedX, fish5SpeedY;
+let fish1Active = true;
+let fish2Active = true;
+let fish3Active = true;
+let fish4Active = true;
+let fish5Active = true;
+let bubble1X, bubble1Y;
+let bubble2X, bubble2Y;
+let bubble1Active = false;
+let bubble2Active = false;
+let bubbleSpeed = 2;
 
 function setup() {
+    //createCanvas(800, 500);
+
+
     let canvas = createCanvas(800, 500);
     canvas.parent("p5-canvas-container");
-
 
     x = width / 2;
     y = height / 2;
@@ -40,16 +52,25 @@ function setup() {
     fish5Y = random(50, 400);
     fish5SpeedX = -random(1, 3);
     fish5SpeedY = random(-1, 1);
+
+    bubble1Y = -1;
+    bubble2Y = -1;
 }
 
 function draw() {
     background(220);
 
     drawBackground();
+    drawBackground();
     drawSeafloor();
     drawCoral();
     drawTallCoral();
     drawFish();
+    drawBubbles();
+
+    fill(255);
+    text("Click the mouse on the fish to eat", 20, 20);
+    text("Press space to exhale bubbles", 20, 40);
 
     if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
         for (let i = 10; i >= 0; i--) {
@@ -59,7 +80,7 @@ function draw() {
 
             drawCreature(x, mouseY, circleSize, waveMove);
         }
-        //     bounce if mouse not on screen
+        //bounce if mouse not on screen
     } else {
         x += xSpeed;
         y += ySpeed;
@@ -83,11 +104,11 @@ function draw() {
 function drawSeafloor() {
     fill(180, 140, 100);
     noStroke();
-    rect(0, height - 80, width, 80); // Sand base
+    rect(0, height - 80, width, 80); // sea floor
 
     fill(160, 120, 80);
     for (let i = 0; i < width; i += 50) {
-        ellipse(i, height - 60, 40, 20); // Small sand mounds
+        ellipse(i, height - 60, 40, 20); // sand mounds
     }
 }
 
@@ -106,7 +127,6 @@ function drawCoral() {
 function drawTallCoral() {
     fill(150, 30, 60);
     for (let i of [30, width - 50]) {
-        // Left and right side coral
         beginShape();
         curveVertex(i, height - 50);
         curveVertex(i - 30, height - 120);
@@ -131,82 +151,255 @@ function drawFish() {
             speedX = fish1SpeedX;
             speedY = fish1SpeedY;
             facingRight = true;
+            if (fish1Active) {
+                ellipse(x, y, 20, 10);
+
+                let tailX, tailY, tailTop, tailBottom, tailEndX;
+
+                if (facingRight) {
+                    tailX = x - 10;
+                    tailEndX = x - 18;
+                } else {
+                    tailX = x + 10;
+                    tailEndX = x + 18;
+                }
+
+                tailY = y;
+                tailTop = y - 5;
+                tailBottom = y + 5;
+
+                triangle(tailX, tailTop, tailX, tailBottom, tailEndX, tailY);
+            }
+            x += speedX;
+            y += speedY;
+
+            // bounce fish off vertical edges
+            if (y < 50 || y > height - 100) {
+                speedY = -speedY;
+            }
+
+            // moves off the edge, reset
+            if (facingRight && x > width) {
+                x = 0;
+                y = random(50, 400);
+            } else if (facingRight == false && x < 0) {
+                x = width;
+                y = random(50, 400);
+            }
+
+            fish1X = x;
+            fish1Y = y;
+            fish1SpeedX = speedX;
+            fish1SpeedY = speedY;
         } else if (i === 2) {
             x = fish2X;
             y = fish2Y;
             speedX = fish2SpeedX;
             speedY = fish2SpeedY;
             facingRight = true;
+            if (fish2Active) {
+                ellipse(x, y, 20, 10);
+
+                let tailX, tailY, tailTop, tailBottom, tailEndX;
+
+                if (facingRight) {
+                    tailX = x - 10;
+                    tailEndX = x - 18;
+                } else {
+                    tailX = x + 10;
+                    tailEndX = x + 18;
+                }
+
+                tailY = y;
+                tailTop = y - 5;
+                tailBottom = y + 5;
+
+                triangle(tailX, tailTop, tailX, tailBottom, tailEndX, tailY);
+            }
+            x += speedX;
+            y += speedY;
+
+            // bounce fish off vertical edges
+            if (y < 50 || y > height - 100) {
+                speedY = -speedY;
+            }
+
+            // moves off the edge, reset
+            if (facingRight && x > width) {
+                x = 0;
+                y = random(50, 400);
+            } else if (facingRight == false && x < 0) {
+                x = width;
+                y = random(50, 400);
+            }
+
+            fish2X = x;
+            fish2Y = y;
+            fish2SpeedX = speedX;
+            fish2SpeedY = speedY;
         } else if (i === 3) {
             x = fish3X;
             y = fish3Y;
             speedX = fish3SpeedX;
             speedY = fish3SpeedY;
             facingRight = true;
+            if (fish3Active) {
+                ellipse(x, y, 20, 10);
+
+                let tailX, tailY, tailTop, tailBottom, tailEndX;
+
+                if (facingRight) {
+                    tailX = x - 10;
+                    tailEndX = x - 18;
+                } else {
+                    tailX = x + 10;
+                    tailEndX = x + 18;
+                }
+
+                tailY = y;
+                tailTop = y - 5;
+                tailBottom = y + 5;
+
+                triangle(tailX, tailTop, tailX, tailBottom, tailEndX, tailY);
+            }
+            x += speedX;
+            y += speedY;
+
+            // bounce fish off vertical edges
+            if (y < 50 || y > height - 100) {
+                speedY = -speedY;
+            }
+
+            // moves off the edge, reset
+            if (facingRight && x > width) {
+                x = 0;
+                y = random(50, 400);
+            } else if (facingRight == false && x < 0) {
+                x = width;
+                y = random(50, 400);
+            }
+
+            fish3X = x;
+            fish3Y = y;
+            fish3SpeedX = speedX;
+            fish3SpeedY = speedY;
         } else if (i == 4) {
             x = fish4X;
             y = fish4Y;
             speedX = fish4SpeedX;
             speedY = fish4SpeedY;
             facingRight = false;
+            if (fish4Active) {
+                ellipse(x, y, 20, 10);
+
+                let tailX, tailY, tailTop, tailBottom, tailEndX;
+
+                if (facingRight) {
+                    tailX = x - 10;
+                    tailEndX = x - 18;
+                } else {
+                    tailX = x + 10;
+                    tailEndX = x + 18;
+                }
+
+                tailY = y;
+                tailTop = y - 5;
+                tailBottom = y + 5;
+
+                triangle(tailX, tailTop, tailX, tailBottom, tailEndX, tailY);
+            }
+            x += speedX;
+            y += speedY;
+
+            // bounce fish off vertical edges
+            if (y < 50 || y > height - 100) {
+                speedY = -speedY;
+            }
+
+            // moves off the edge, reset
+            if (facingRight && x > width) {
+                x = 0;
+                y = random(50, 400);
+            } else if (facingRight == false && x < 0) {
+                x = width;
+                y = random(50, 400);
+            }
+
+            fish4X = x;
+            fish4Y = y;
+            fish4SpeedX = speedX;
+            fish4SpeedY = speedY;
         } else if (i == 5) {
             x = fish5X;
             y = fish5Y;
             speedX = fish5SpeedX;
             speedY = fish5SpeedY;
             facingRight = false;
-        }
+            if (fish5Active) {
+                ellipse(x, y, 20, 10);
 
-        ellipse(x, y, 20, 10);
+                let tailX, tailY, tailTop, tailBottom, tailEndX;
 
-        let tailX = x + (facingRight ? -10 : 10);
-        let tailY = y;
-        let tailTop = y - 5;
-        let tailBottom = y + 5;
-        let tailEndX = x + (facingRight ? -18 : 18);
+                if (facingRight) {
+                    tailX = x - 10;
+                    tailEndX = x - 18;
+                } else {
+                    tailX = x + 10;
+                    tailEndX = x + 18;
+                }
 
-        triangle(tailX, tailTop, tailX, tailBottom, tailEndX, tailY);
+                tailY = y;
+                tailTop = y - 5;
+                tailBottom = y + 5;
 
-        x += speedX;
-        y += speedY;
+                triangle(tailX, tailTop, tailX, tailBottom, tailEndX, tailY);
+            }
+            x += speedX;
+            y += speedY;
 
-        // bounce fish off vertical edges
-        if (y < 50 || y > height - 100) speedY *= -1;
+            // bounce fish off vertical edges
+            if (y < 50 || y > height - 100) {
+                speedY = -speedY;
+            }
 
-        // moves off the edge, reset
-        if (facingRight && x > width) {
-            x = 0;
-            y = random(50, 400);
-        } else if (!facingRight && x < 0) {
-            x = width;
-            y = random(50, 400);
-        }
+            // moves off the edge, reset
+            if (facingRight && x > width) {
+                x = 0;
+                y = random(50, 400);
+            } else if (facingRight == false && x < 0) {
+                x = width;
+                y = random(50, 400);
+            }
 
-        if (i === 1) {
-            fish1X = x;
-            fish1Y = y;
-            fish1SpeedX = speedX;
-            fish1SpeedY = speedY;
-        } else if (i === 2) {
-            fish2X = x;
-            fish2Y = y;
-            fish2SpeedX = speedX;
-            fish2SpeedY = speedY;
-        } else if (i === 3) {
-            fish3X = x;
-            fish3Y = y;
-            fish3SpeedX = speedX;
-            fish3SpeedY = speedY;
-        } else if (i === 4) {
-            fish4X = x;
-            fish4Y = y;
-            fish4SpeedX = speedX;
-            fish4SpeedY = speedY;
-        } else if (i === 5) {
             fish5X = x;
             fish5Y = y;
             fish5SpeedX = speedX;
             fish5SpeedY = speedY;
+        }
+    }
+}
+
+function drawBubbles() {
+    fill(173, 216, 230, 150);
+    noStroke();
+
+    if (bubble1Active) {
+        ellipse(bubble1X, bubble1Y, 15, 20);
+        ellipse(bubble1X + 15, bubble1Y + 5, 15, 15);
+        bubble1Y -= bubbleSpeed;
+
+        if (bubble1Y < 0) {
+            bubble1Active = false;
+        }
+    }
+
+    if (bubble2Active) {
+        ellipse(bubble2X, bubble2Y, 20, 20);
+        ellipse(bubble2X + 15, bubble2Y + 5, 15, 15);
+        bubble2Y -= bubbleSpeed;
+
+        if (bubble2Y < 0) {
+            bubble2Active = false;
         }
     }
 }
@@ -317,4 +510,49 @@ function drawCreature(x, y, circleSize, waveMove) {
     circle(0, yOffset, circleSize * breathe);
 
     pop();
+}
+
+function mouseClicked() {
+    for (let i = 1; i <= 5; i++) {
+        let x, y;
+
+        if (i === 1) {
+            x = fish1X;
+            y = fish1Y;
+        } else if (i === 2) {
+            x = fish2X;
+            y = fish2Y;
+        } else if (i === 3) {
+            x = fish3X;
+            y = fish3Y;
+        } else if (i === 4) {
+            x = fish4X;
+            y = fish4Y;
+        } else if (i === 5) {
+            x = fish5X;
+            y = fish5Y;
+        }
+
+        if (dist(mouseX, mouseY, x, y) < 50) {
+            if (i === 1) fish1Active = false;
+            else if (i === 2) fish2Active = false;
+            else if (i === 3) fish3Active = false;
+            else if (i === 4) fish4Active = false;
+            else if (i === 5) fish5Active = false;
+        }
+    }
+}
+
+function keyPressed() {
+    if (key === " ") {
+        if (!bubble1Active) {
+            bubble1X = mouseX;
+            bubble1Y = mouseY;
+            bubble1Active = true;
+        } else if (!bubble2Active) {
+            bubble2X = mouseX;
+            bubble2Y = mouseY;
+            bubble2Active = true;
+        }
+    }
 }
